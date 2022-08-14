9ca3964818a8949456c9eda3237626d8bc605920
siqiou <108245432+siqiou@users.noreply.github.com>
2022-08-13 22:01:43 -0400

    Update Employee_Database_challenge.sql

--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -1,1 +1,48 @@
+--Deliverable 1 
+--Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+--Retrieve the title, from_date, and to_date columns from the Titles table.
+--Create a new table using the INTO clause.
+--Join both tables on the primary key.
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+SELECT * from retirement_titles;
+
+-- Retrieve the employee number, first and last name, and title columns from the Retirement Titles table
+-- Use Dictinct to remove duplicates
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, to_date DESC;
+
+SELECT * from unique_titles;
+
+
+-- Retrieve the number of employees by their most recent job title who are about to retire
+-- Group the table by title, then sort the count column in descending order.
+
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+SELECT * from retiring_titles;
+
 
