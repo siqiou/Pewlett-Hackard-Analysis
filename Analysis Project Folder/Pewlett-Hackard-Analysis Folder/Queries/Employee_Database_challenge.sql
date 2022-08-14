50a7301227ac50fe6bb48a764cdbfe56b70356a6
siqiou <108245432+siqiou@users.noreply.github.com>
2022-08-13 22:11:55 -0400

    Update Employee_Database_challenge.sql

--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -56,13 +56,13 @@ SELECT DISTINCT ON(e.emp_no) e.emp_no,
     de.to_date,
     t.title
 INTO mentorship_eligibilty
-
 FROM employees as e
 Left outer Join dept_emp as de
-ON (e.emp_no = de.emp_no)
+       ON (e.emp_no = de.emp_no)
 Left outer Join titles as t
-ON (e.emp_no = t.emp_no)
+       ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
 
 SELECT * from mentorship_eligibilty;
