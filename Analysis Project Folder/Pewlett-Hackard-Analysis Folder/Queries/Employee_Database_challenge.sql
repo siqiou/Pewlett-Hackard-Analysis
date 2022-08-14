5c82874465322c08fe8cac00d9052684d2c01061
siqiou <108245432+siqiou@users.noreply.github.com>
2022-08-13 22:09:26 -0400

    Update Employee_Database_challenge.sql

--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -45,4 +45,24 @@ ORDER BY COUNT(title) DESC;
 
 SELECT * from retiring_titles;
 
+--Deliverable 2
+--Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
 
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * from mentorship_eligibilty;
